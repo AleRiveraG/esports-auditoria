038853355a3978369068920912cc55bfbd79b383
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-09 19:07:50 -0400

    Actualización de Repository para usar JpaRepository

--- src/main/java/com/auditoria/microservicio_auditoria/repository/AuditoriaRepository.java
+++ src/main/java/com/auditoria/microservicio_auditoria/repository/AuditoriaRepository.java
@@ -1,34 +1,11 @@
 package com.auditoria.microservicio_auditoria.repository;
 
 import com.auditoria.microservicio_auditoria.model.Auditoria;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class AuditoriaRepository {
-
-    private List<Auditoria> listaAuditoria = new ArrayList<>();
-
-    public List<Auditoria> obtenerTodos(){
-        return listaAuditoria;
-    }
-
-    public Auditoria buscarPorId(Long id){
-        for(Auditoria auditoria: listaAuditoria){
-            if(auditoria.getIdAuditoria().equals(id)){
-                return auditoria;
-            }
-        }
-        return null;
-    }
-    public Auditoria generarAuditoria(Auditoria auditoria){
-        listaAuditoria.add(auditoria);
-        return auditoria;
-    }
-
-
+public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
 
 
 }
