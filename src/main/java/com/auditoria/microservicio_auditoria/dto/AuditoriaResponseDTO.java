9dc8ab21aeb5ebcf35e9ece0757356bd15e75511
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-09 19:24:52 -0400

    Actualización de Service

--- src/main/java/com/auditoria/microservicio_auditoria/dto/AuditoriaResponseDTO.java
+++ src/main/java/com/auditoria/microservicio_auditoria/dto/AuditoriaResponseDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class AuditoriaResponseDTO {
 
+    private Long idAuditoria;
     private String detalle;
     private LocalDate fecha;
 
--- src/main/java/com/auditoria/microservicio_auditoria/service/AuditoriaService.java
+++ src/main/java/com/auditoria/microservicio_auditoria/service/AuditoriaService.java
@@ -1,27 +1,45 @@
 package com.auditoria.microservicio_auditoria.service;
 
+import com.auditoria.microservicio_auditoria.dto.AuditoriaRequestDTO;
+import com.auditoria.microservicio_auditoria.dto.AuditoriaResponseDTO;
 import com.auditoria.microservicio_auditoria.model.Auditoria;
 import com.auditoria.microservicio_auditoria.repository.AuditoriaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class AuditoriaService {
 
-    @Autowired
-    private AuditoriaRepository auditoriaRepository;
+    private final AuditoriaRepository auditoriaRepository;
 
-    public List<Auditoria> obtenerTodos(){
-        return auditoriaRepository.obtenerTodos();
+    public AuditoriaResponseDTO mapToDTO(Auditoria auditoria){
+        return new AuditoriaResponseDTO(
+                auditoria.getIdAuditoria(),
+                auditoria.getDetalle(),
+                auditoria.getFecha()
+        );
     }
 
-    public Auditoria buscarPorId(Long id){
-        return auditoriaRepository.buscarPorId(id);
+    public List<AuditoriaResponseDTO> obtenerTodos(){
+        return auditoriaRepository.findAll()
+                .stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
     }
 
-    public Auditoria generarAuditoria(Auditoria auditoria){
-        return auditoriaRepository.generarAuditoria(auditoria);
+    public Optional<AuditoriaResponseDTO> buscarPorId(Long id){
+        return auditoriaRepository.findById(id).map(this::mapToDTO);
     }
+
+    public AuditoriaResponseDTO generarAuditoria(AuditoriaRequestDTO auditoria){
+        Auditoria auditoria1 = new Auditoria(null, auditoria.getDetalle(), auditoria.getFecha());
+        return mapToDTO(auditoriaRepository.save(auditoria1));
+    }
+
 }
